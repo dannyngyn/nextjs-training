f00c0e49959dfe6536da77ff92824b7e103da77e
unknown <danny.nguyen772@gmail.com>
2024-09-16 13:11:35 -0500

    Section-3
    039 - applied useFormStatus to button

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,6 +1,7 @@
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 import { shareMeal } from '@/lib/action';
+import MealFormSubmit from '@/components/meals/meal-form-submit';
 
 export default function ShareMealPage() {
 
@@ -43,7 +44,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealFormSubmit/>
           </p>
         </form>
       </main>
