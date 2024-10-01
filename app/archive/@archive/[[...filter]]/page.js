9531eda021c4897760350aa6aa9e18e83b8a1e92
unknown <danny.nguyen772@gmail.com>
2024-10-01 08:50:45 -0500

    Section-4
    011 - Throwing errors

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -25,6 +25,11 @@ export default function ArchiveYear({params}){
     newsContent = <NewsList news={news}/>
   }
 
+  if (selectedYear && !getAvailableNewsYears().includes(selectedYear) ||
+      selectedMonth && !getAvailableNewsMonths(selectedYear).includes(selectedMonth)){
+    throw new Error('Invalid filter.')
+  }
+
   return (
     <>
       <header id="archive-header">
